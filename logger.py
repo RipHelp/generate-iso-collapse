3622484162b07840377628f94c5e99748eecb90e
Vladislav Mironov <vladislav.mironov9524@gmail.com>
2024-02-13 22:10:43 +0300

    add primitive logger

---
+++ logger.py
@@ -0,0 +1,33 @@
+"""
+    Простой логгер
+"""
+
+import sys
+import logging
+
+ENCODING_UTF8 = "utf-8"
+LOG_FILE_NAME = "log_info.log"
+
+
+class LogHelper:
+
+    def __init__(self):
+        logging.basicConfig(level=logging.INFO, filename=LOG_FILE_NAME, encoding=ENCODING_UTF8)
+        logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+        self.save_generate_info()
+
+    def log_info(self, message: str):
+        return logging.info(message)
+
+    def log_error(self, message: str):
+        return logging.error(message)
+
+    def save_generate_info(self, iso_name: str = 'iso_name', iso_size: float = 'iso_size', md5sum: hex = 'md5sum') -> bool:
+        """Сохранение сгенерированной информации в файл"""
+        with open("generate_info.txt", "a") as generate_log:
+            if not generate_log.write(f"{iso_name}<>{iso_size}<>{md5sum}\n"):
+                return False
+        return True
+
+
