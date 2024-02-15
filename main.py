60dfcf6206d35dc907c39892fe44c951ea175f08
Vladislav Mironov <vladislav.mironov9524@gmail.com>
2024-02-15 12:42:35 +0300

    add main

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+import argparse
+
+from tools import Tools
+from logger import LogHelper
+from collapse_generate import GenerateCollapse
+
+
+def parse_args() -> argparse.Namespace:
+    parser = create_parser()
+    return parser.parse_args(sys.argv[1:])
+
+
+def create_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-path', nargs=1, help='Полный путь')
+    parser.add_argument('-percent', nargs=1, help='Процентное заполнение')
+    return parser
+
+
+if __name__ == '__main__':
+
+    args = parse_args()
+
+    Log = LogHelper()
+    Tools = Tools(log=Log, path=args.path[0], percent=args.percent[0])
+    if not Tools.verify_args():
+        Log.log_error("Получены не валидные аргументы")
+    GenerateCollapse = GenerateCollapse(log=Log, tools=Tools)
+
+    while True:
+        GenerateCollapse.generate_collapse()
+        if Tools.checker_size(path=args.path[0]):
+            break
+
+
+
